811d7d2373c7d70bb0692bd8c1fb1ef6389bef0b
JenJordyn <JenniferBridges02@gmail.com>
2025-02-02 18:16:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -3,59 +3,42 @@
 // Function to handle button click events
 function selectOption(option) {
     if (option === 'yes') {
+        // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
-            displayCatHeart(); // Display the cat-heart.gif
+            displayHeartImage(); // Display the heart image
         });
     } else if (option === 'no') {
-        document.getElementById('no-button').innerText = 'Erm you sure?'; // Change text on "No" button
+        // Change text on the "No" button to "Erm you sure?"
+        document.getElementById('no-button').innerText = 'Erm you sure?';
+        
+        // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
-        var newSize = parseFloat(currentFontSize) * 2; // Increase font size by 2x
+        var newSize = parseFloat(currentFontSize) * 2; // Increase font size by * 2
         yesButton.style.fontSize = newSize + 'px';
     }
 }
 
-// Function to flash rainbow colors
+// Function to flash rainbow colors and then execute a callback function
 function flashRainbowColors(callback) {
     var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
     var i = 0;
     var interval = setInterval(function() {
         document.body.style.backgroundColor = colors[i];
         i = (i + 1) % colors.length;
-    }, 200);
+    }, 200); // Change color every 200 milliseconds
     setTimeout(function() {
         clearInterval(interval);
-        document.body.style.backgroundColor = ''; 
+        document.body.style.backgroundColor = ''; // Reset background color
         if (callback) {
             callback();
         }
-    }, 2000);
+    }, 2000); // Flash colors for 2 seconds
 }
 
-// Function to display 2.png initially
-function displayImage() {
-    var imageContainer = document.getElementById('image-container');
-    var img = new Image();
-    img.src = '2.png'; // Replace cat.gif with 2.png
-    img.alt = 'Cute Image';
-    img.onload = function() {
-        imageContainer.appendChild(img);
-    };
+// Function to display the heart image when "Yes" is clicked
+function displayHeartImage() {
+    document.getElementById('image-container').innerHTML = '<img src="cat-heart.gif" alt="Cat Heart" style="width: 80px; height: auto;">';
+    document.getElementById('options').style.display = 'none';
 }
-
-// Function to display the cat-heart.gif
-function displayCatHeart() {
-    document.getElementById('image-container').innerHTML = '';
-    var imageContainer = document.getElementById('image-container');
-    var catHeartImage = new Image();
-    catHeartImage.src = 'cat-heart.gif';
-    catHeartImage.alt = 'Cat Heart';
-    catHeartImage.onload = function() {
-        imageContainer.appendChild(catHeartImage);
-        document.getElementById('options').style.display = 'none';
-    };
-}
-
-// Display 2.png initially
-displayImage();
